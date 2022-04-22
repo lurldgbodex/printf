c5ea9c3d31dfe36a06b17aaddbb6fab654d62a9f
exlibrischimex <exlibrischimex@gmail.com>
2022-04-22 13:16:51 -0700

    modified code

--- get_print.c
+++ get_print.c
@@ -2,10 +2,12 @@
 
 /**
  * get_print - selects the right printing function
- * depending on the conversion specifier
- * @s: character that hold the conversion specifier 
- * Description: the function loops throught the structs array
- *
+ * depending on the conversion specifier passed to _printf
+ * @s: character that holds the conversion specifier
+ * Description: the function loops through the structs array
+ * func_arr[] to find a match between the specifier passed to _printf
+ * and the first element of the struct, and then the approriate
+ * printing function
  * Return: a pointer to the matching printing function
  */
 int (*get_print(char s))(va_list, flags_t *)
@@ -14,17 +16,18 @@ int (*get_print(char s))(va_list, flags_t *)
 		{'i', print_int},
 		{'s', print_string},
 		{'c', print_char},
-		{'d', print_unsigned},
+		{'d', print_int},
+		{'u', print_unsigned},
 		{'x', print_hex},
 		{'X', print_hex_big},
-		{'%', print_percent},
-		{'p', print_address},
-		{'S', print_bigS},
-		{'r', print_rev},
+		{'b', print_binary},
 		{'o', print_octal},
 		{'R', print_rot13},
-		{'0', print_octal}
-	};
+		{'r', print_rev},
+		{'S', print_bigS},
+		{'p', print_address},
+		{'%', print_percent}
+		};
 	int flags = 14;
 
 	register int i;
